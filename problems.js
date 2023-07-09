370213caf291c01340f066b5441b54219501f4af
guantanamobosch <luketyson17@gmail.com>
2023-07-09 14:30:11 -0600

    compare triplets

--- problems.js
+++ problems.js
@@ -8,3 +8,18 @@
 //     const sum = arr.reduce((sum, num) => sum + num, 0);
 //     return sum;
 // }
+
+// 🤌🏼 Compare Triplets
+// function compareTriplets(a, b) {
+//     let score = [0, 0];
+
+//     for (let i = 0; i < a.length; i++) {
+//         if (a[i] === b[i]) {
+//             continue;
+//         } else if (a[i] > b[i]) {
+//             score[0] += 1;
+//         } else score[1] += 1;
+//     }
+
+//     return score;
+// }
